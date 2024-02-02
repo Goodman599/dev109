7e02d18596de148d04437676d347c5e7e1d3f01b
Goodman599 <Gonghm2007@gmail.com>
2024-02-01 17:17:06 -0800

    Made the "3" an integer

--- Modules/m5/bullseye.js
+++ Modules/m5/bullseye.js
@@ -1,4 +1,4 @@
-var table = prompt("Please enter the table", "3");;             // Unit of table
+var table = prompt("Please enter the table", 3);;             // Unit of table
 var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
