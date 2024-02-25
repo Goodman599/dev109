7f9a5571cf2b26bb13a0e1ddb718476900ee2edd
Goodman599 <Gonghm2007@gmail.com>
2024-02-24 18:58:39 -0800

    Fixed lots of shit

--- Artifacts/a4/validator.js
+++ Artifacts/a4/validator.js
@@ -1,20 +1,27 @@
-function isValid() {
+// Array to store the validity of each field
+var checks = [false, false, false, false, false, false, false, false, false];
 
-    event.preventDefault();
+function isValid() { // There is a evaluate parameter to prevent infinite loops caused by checks by blur events
+    names(0);
+    names(1);
+    email();
+    phone();
+    username();
+    password();
+    addressOrCity("address");
+    addressOrCity("city");
+    zip();
 
-    // I am not putting all of them in a single if statement with &&
-    // This is because it stops evaluating the rest of the checks if one fails
+    // check every item in checks
     var valid = true;
-    if (!names(0)) { valid = false; }
-    if (!names(1)) { valid = false; }
-    if (!email()) { valid = false; }
-    if (!phone()) { valid = false; }
-    if (!username()) { valid = false; }
-    if (!password()) { valid = false; }
-
-    console.log(/^[A-Z]+$/.test("asdAasd"))
+    for (var i in checks) {
+        if (!checks[i]) {
+            valid = false;
+        }
+    }
 
     if (valid) {
+        document.getElementById("submitError").innerHTML = "";
         return true;
     }
     else {
@@ -24,15 +31,19 @@ function isValid() {
     }
 }
 
-document.getElementById("firstName").addEventListener('blur', function () { names(0) }, false);
-document.getElementById("lastName").addEventListener('blur', function () { names(1) }, false);
-document.getElementById("email").addEventListener('blur', function () { email() }, false);
-document.getElementById("phone").addEventListener('blur', function () { phone() }, false);
-document.getElementById("username").addEventListener('blur', function () { username() }, false);
-document.getElementById("password").addEventListener('blur', function () { password() }, false);
+// Add event listeners that all have the CalledByBlur set to true
+document.getElementById("firstName").addEventListener('blur', function () { names(0, true) }, false);
+document.getElementById("lastName").addEventListener('blur', function () { names(1, true) }, false);
+document.getElementById("email").addEventListener('blur', function () { email(true) }, false);
+document.getElementById("phone").addEventListener('blur', function () { phone(true) }, false);
+document.getElementById("username").addEventListener('blur', function () { username(true) }, false);
+document.getElementById("password").addEventListener('blur', function () { password(true) }, false);
+document.getElementById("address").addEventListener('blur', function () { addressOrCity("address", true) }, false);
+document.getElementById("city").addEventListener('blur', function () { addressOrCity("city", true) }, false);
+document.getElementById("zip").addEventListener('blur', function () { zip(true) }, false);
 
 
-function names(nameType) {
+function names(nameType, calledByBlur = false) {
     // Make an array that converts 0s and 1s into first name/last name and their maximum lengths
     const nameData = [["first", 20], ["last", 50]];
 
@@ -59,16 +70,21 @@ function names(nameType) {
     } else {
         validName = true;
         console.log(nameData[nameType][0] + " name valid");
+
+        if (calledByBlur) {
+            // Run isValid again in case everything has been solved and thus the big error at the bottom should be removed
+            isValid();
+        }
     }
 
     //4) Send error message to HTML
     document.getElementById(nameData[nameType][0].charAt(0) + "nameError").innerHTML = errorMessages;
 
-    //5) return status of each field
-    return (validName);
+    //5) Updates the checks array
+    checks[nameType] = validName;
 }
 
-function email() {
+function email(calledByBlur = false) {
     var userEmail = document.getElementById("email").value;
     var atpos = userEmail.indexOf("@");
     var dotpos = userEmail.lastIndexOf(".");
@@ -76,65 +92,163 @@ function email() {
     if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length) {
         document.getElementById("emailError").innerHTML = "<p>Invalid email </p>";
         console.log("Invalid email");
+        checks[2] = false;
+
     } else {
         document.getElementById("emailError").innerHTML = "";
         console.log("Email valid");
-        return true;
+
+        checks[2] = true;
+
+        if (calledByBlur) {
+            // Run isValid again in case everything has been solved and thus the big error at the bottom should be removed
+            isValid();
+        }
     }
 }
 
-function phone() {
+function phone(calledByBlur = false) {
     var phone = document.getElementById("phone").value;
     if (isNaN(phone) || phone.length > 15 || phone === null || phone === "") {
         document.getElementById("phoneError").innerHTML = "<p>Invalid phone number </p>";
+        console.log("Invalid phone number")
+        checks[3] = false;
+
     } else {
         document.getElementById("phoneError").innerHTML = "";
         console.log("Phone number valid");
-        return true;
+
+        checks[3] = true;
+
+        if (calledByBlur) {
+            // Run isValid again in case everything has been solved and thus the big error at the bottom should be removed
+            isValid();
+        }
     }
 }
 
-function username() {
+function username(calledByBlur = false) {
     var username = document.getElementById("username").value;
     if (username === "null" || username === "" || username.length > 12) {
         document.getElementById("usernameError").innerHTML = "<p>The username is required and cannot be greater than 12 characters</p>";
         console.log("username invalid — length");
-    }
-    else {
+        checks[4] = false;
+
+    } else {
         document.getElementById("usernameError").innerHTML = "";
         console.log("Username valid");
-        return true;
+
+        checks[4] = true;
+
+        if (calledByBlur) {
+            // Run isValid again in case everything has been solved and thus the big error at the bottom should be removed
+            isValid();
+        }
     }
 }
 
-function password() {
+function password(calledByBlur = false) {
     var password = document.getElementById("password").value;
     if (password === "null" || password === "" || password.length > 17) {
         document.getElementById("passwordError").innerHTML = "<p>The password is required and cannot be greater than 17 characters</p>";
         console.log("password invalid — length");
+        checks[5] = false;
 
     } else if (/[A-Z]/g.test(password) == false) { // test for capital letters
         document.getElementById("passwordError").innerHTML = "<p>The password requires at least 1 uppercase letter</p>";
         console.log("password invalid — uppercase");
+        checks[5] = false;
 
     } else if (/[a-z]/g.test(password) == false) { // test for lowercase letters
         document.getElementById("passwordError").innerHTML = "<p>The password requires at least 1 lowercase letter</p>";
         console.log("password invalid — lowercase");
+        checks[5] = false;
 
     } else if (/[1-9]/g.test(password) == false) { // test for number
         document.getElementById("passwordError").innerHTML = "<p>The password requires at least 1 number</p>";
         console.log("password invalid — number");
+        checks[5] = false;
 
     } else if (/[!@#$%^&* ()_ +\-=\[\]{ }; ':\"\\|,.<>\/?]/g.test(password) == false) { // test for special characters
         document.getElementById("passwordError").innerHTML = "<p>The password requires at least 1 special character</p>";
         console.log("password invalid — special character");
+        checks[5] = false;
 
     } else {
         document.getElementById("passwordError").innerHTML = "";
         console.log("Password valid");
-        return true;
+
+        checks[5] = true;
+
+        if (calledByBlur) {
+            // Run isValid again in case everything has been solved and thus the big error at the bottom should be removed
+            isValid();
+        }
     }
 }
+
+function addressOrCity(locationType, calledByBlur = false) {
+    var location = document.getElementById(locationType).value;
+    // Store the position this has in the array based on which data point this is
+    var checkPos = 6;
+    if (locationType == "city") {
+        checkPos = 7;
+    }
+
+    if (location === "null" || location === "") {
+        document.getElementById(locationType + "Error").innerHTML = "<p>The " + locationType + " is required</p>";
+        console.log(locationType + " invalid — length");
+        checks[checkPos] = false;
+
+    } else {
+        document.getElementById(locationType + "Error").innerHTML = "";
+        console.log(locationType + " valid");
+
+        checks[checkPos] = true;
+
+        if (calledByBlur) {
+            // Run isValid again in case everything has been solved and thus the big error at the bottom should be removed
+            isValid();
+        }
+    }
+}
+
+function zip(calledByBlur = false) {
+    var zip = document.getElementById("zip").value;
+
+    // Make sure the ZIP is shown
+    if (document.getElementById("zip").className == "hide") {
+        console.log("No zip needed");
+
+        checks[8] = true;
+
+        if (calledByBlur) {
+            // Run isValid again in case everything has been solved and thus the big error at the bottom should be removed
+            isValid();
+        }
+
+        return;
+    }
+
+    if (zip === "null" || zip === "") {
+        document.getElementById("zipError").innerHTML = "<p>The zip is required</p>";
+        console.log("zip invalid — length");
+        checks[8] = false;
+
+    }
+    else {
+        document.getElementById("zipError").innerHTML = "";
+        console.log("Zip valid");
+
+        checks[8] = true;
+
+        if (calledByBlur) {
+            // Run isValid again in case everything has been solved and thus the big error at the bottom should be removed
+            isValid();
+        }
+    }
+}
+
 
 // Run showZip to hide the zip code
 showZip();
@@ -144,14 +258,19 @@ function showZip() {
 
     var country = document.getElementById("country").value;
     var zipLabel = document.getElementById("zipLabel");
-    var zip = document.getElementById("zip");
+    var zipCode = document.getElementById("zip");
     if (country == "United States") {
         console.log("Showing ZIP");
         zipLabel.className = "";
-        zip.className = "";
+        zipCode.className = "";
+        // Re-evaluate zip() after showing/hiding
+        zip();
     } else {
         console.log("Hiding ZIP");
         zipLabel.className = "hide";
-        zip.className = "hide";
+        zipCode.className = "hide";
+        // Re-evaluate zip() after showing/hiding
+        zip();
     }
 }
+
