a03860bb9895bdb7f491ccf93dfc84c2be6de19b
Goodman599 <Gonghm2007@gmail.com>
2024-03-02 14:27:28 -0800

    Added descriptions

--- Artifacts/a5/cycle-images.js
+++ Artifacts/a5/cycle-images.js
@@ -1,4 +1,13 @@
 var current = 1;
+var descriptions = [
+    "This is the first random image I found. Its got some cool bubbles.",
+    "Fun fact:<br>This was the smallest image out of the five, so the other four were shrunk to match this one. Not only that, they are then enlarged again to fit the website. Not my proudest idea...",
+    "This was originally a cool background but it got cropped so much it's just a normal pattern now.",
+    "This is image 4. I think it's supposed to be a cool data/code-y thing. I think I cropped it quite well.",
+    "This image is my personal favorite. The metallic patterns look really nice."
+];
+
+
 
 // Change Images
 function cycleImage(amount = 1) {
@@ -9,6 +18,7 @@ function cycleImage(amount = 1) {
     else if (current > 5) current = 1;
 
     document.getElementsByTagName("img")[0].src = "images/" + current + ".jpg";
+    document.getElementsByTagName("p")[0].innerHTML = descriptions[current - 1];
 }
 
 // Add event listeners to buttons
